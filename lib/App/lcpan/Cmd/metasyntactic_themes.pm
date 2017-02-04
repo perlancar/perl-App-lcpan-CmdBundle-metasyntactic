dba5cb9b37eb83127fc0bfa9a7c0bba2958bb504
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2017-02-04 07:58:51 +0700

    Initial commit

---
+++ lib/App/lcpan/Cmd/metasyntactic_themes.pm
@@ -0,0 +1,44 @@
+package App::lcpan::Cmd::metasyntactic_themes;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+use Log::Any::IfLOG '$log';
+
+require App::lcpan;
+
+our %SPEC;
+
+$SPEC{handle_cmd} = {
+    v => 1.1,
+    summary => 'List all Acme::MetaSyntactic theme modules',
+    args => {
+        %App::lcpan::common_args,
+        %App::lcpan::detail_args,
+    },
+};
+sub handle_cmd {
+    my %args = @_;
+    my $res = App::lcpan::modules(%args, namespaces => ['Acme::MetaSyntactic']);
+    return $res unless $res->[0] == 200;
+    my @fres;
+    for my $item (@{ $res->[2] }) {
+        my $mod = $args{detail} ? $item->{module} : $item;
+        next unless $mod =~ /^Acme::MetaSyntactic::[a-z0-9]/;
+        if ($args{detail}) {
+            ($item->{theme} = $mod) =~ s/^Acme::MetaSyntactic:://;
+        } else {
+            $item =~ s/^Acme::MetaSyntactic:://;
+        }
+        push @fres, $item;
+    }
+    $res->[2] = \@fres;
+    unshift @{ $res->[3]{'table.fields'} }, 'theme';
+    $res;
+}
+
+1;
+# ABSTRACT:
---
+++ lib/App/lcpan/CmdBundle/metasyntactic.pm
@@ -0,0 +1,19 @@
+package App::lcpan::CmdBundle::metasyntactic;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: More lcpan subcommands related to Acme::MetaSyntactic
+
+=head1 SYNOPSIS
+
+
+=head1 DESCRIPTION
+
+
+=head1 SEE ALSO
+
+L<lcpan>
+
+L<Acme::MetaSyntactic>
